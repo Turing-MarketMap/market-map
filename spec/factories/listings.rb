1739a5130a065c3d3d93561eda622e54f2e2f59d
Kevin Gloss <kgloss89@gmail.com>
2022-02-25 12:07:04 -0700

    Update listing factory faker data

--- spec/factories/listings.rb
+++ spec/factories/listings.rb
@@ -1,18 +1,17 @@
 FactoryBot.define do
   factory :listing do
-    year { Faker::Commerce.product_name }
-    make { Faker::Lorem.sentence }
-    model { Faker::Commerce.price}
-    trim { Faker::Commerce.price}
-    body { Faker::Commerce.price}
-    transmission { Faker::Commerce.price}
-    vin { Faker::Commerce.price}
-    state { Faker::Commerce.price}
-    condition { Faker::Commerce.price}
-    odometer { Faker::Commerce.price}
-    color { Faker::Commerce.price}
-    interior { Faker::Commerce.price}
-    sellingprice { Faker::Commerce.price}
-
+    year { Faker::Vehicle.year }
+    make { Faker::Vehicle.make }
+    model { Faker::Vehicle.model(make) }
+    trim { Faker::Vehicle.style }
+    body { Faker::Vehicle.car_type }
+    transmission { Faker::Vehicle.transmission }
+    vin { Faker::Vehicle.vin }
+    state { Faker::Address.state_abbr }
+    condition { Faker::Vehicle.} 
+    odometer { Faker::Vehicle.milage }
+    color { Faker::Vehicle.color }
+    interior { Faker::Lorem.sentence }
+    sellingprice { Faker::Commerce.price }
   end
 end
