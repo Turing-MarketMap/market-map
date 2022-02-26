fb5e8100a84d8ecd80402d7e12e8ba9d5621b623
Croix Kimmel <croixkimmel@gmail.com>
2022-02-26 11:06:08 -0700

    Add tests for filters, filter by year code

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,16 @@
 class Listing < ApplicationRecord
+  def self.filter_by_year(min_year, max_year)
+    filtered = Listing.where(year: min_year..max_year)
+  end
+
+  def self.filter_by_make(make)
+    filtered = Listing.where(make: make)
+  end
+
+  def self.filter_by_model(model)
+    filtered = Listing.where(model: model)
+  end
+
 
 
 end
--- spec/models/listing_spec.rb
+++ spec/models/listing_spec.rb
@@ -2,27 +2,54 @@ require 'rails_helper'
 
 RSpec.describe Listing do
   before(:each) do
-    @listing = create(:listing)
+    # @listing = create(:listing)
+
+    @listing_1 = create(:listing, year: 2001, make: "Toyota", model: "Camry")
+    @listing_2 = create(:listing, year: 2002, make: "Toyota", model: "4Runner")
+    @listing_3 = create(:listing, year: 2003, make: "Toyota", model: "Tundra")
+    @listing_4 = create(:listing, year: 2004, make: "Chevrolet", model: "Suburban")
+    @listing_5 = create(:listing, year: 2005, make: "Chevrolet", model: "Silverado")
+    @listing_6 = create(:listing, year: 2006, make: "Ford", model: "F-150")
+    @listing_7 = create(:listing, year: 2007, make: "Ford", model: "F-150")
+    @listing_8 = create(:listing, year: 2008, make: "Ford", model: "Focus")
+    @listing_9 = create(:listing, year: 2009, make: "Ford", model: "Escape")
+    @listing_10 = create(:listing, year: 2010, make: "Ford", model: "Escape")
+
   end
 
   it 'exists' do
-    expect(@listing).to be_a(Listing)
+    expect(@listing_1).to be_a(Listing)
   end
 
   it 'has attributes' do
-    expect(@listing.id).to be_a(Integer)
-    expect(@listing.year).to be_a(Integer)
-    expect(@listing.make).to be_a(String)
-    expect(@listing.model).to be_a(String)
-    expect(@listing.trim).to be_a(String)
-    expect(@listing.body).to be_a(String)
-    expect(@listing.transmission).to be_a(String)
-    expect(@listing.vin).to be_a(String)
-    expect(@listing.state).to be_a(String)
-    expect(@listing.condition).to be_a(String)
-    expect(@listing.odometer).to be_a(Integer)
-    expect(@listing.color).to be_a(String)
-    expect(@listing.interior).to be_a(String)
-    expect(@listing.sellingprice).to be_a(Integer)
+    expect(@listing_1.id).to be_a(Integer)
+    expect(@listing_1.year).to be_a(Integer)
+    expect(@listing_1.make).to be_a(String)
+    expect(@listing_1.model).to be_a(String)
+    expect(@listing_1.trim).to be_a(String)
+    expect(@listing_1.body).to be_a(String)
+    expect(@listing_1.transmission).to be_a(String)
+    expect(@listing_1.vin).to be_a(String)
+    expect(@listing_1.state).to be_a(String)
+    expect(@listing_1.condition).to be_a(String)
+    expect(@listing_1.odometer).to be_a(Integer)
+    expect(@listing_1.color).to be_a(String)
+    expect(@listing_1.interior).to be_a(String)
+    expect(@listing_1.sellingprice).to be_a(Integer)
+  end
+
+  it 'can filter by year' do
+    filtered = Listing.filter_by_year(2003,2006)
+    expect(filtered.count).to eq(4)
+  end
+
+  it 'can filter by make' do
+    fords = Listing.filter_by_make("Ford")
+    expect(fords.count).to eq(5)
+  end
+
+  it 'can filter by model' do
+    f150s = Listing.filter_by_model("F-150")
+    expect(f150s.count).to eq(2)
   end
 end
