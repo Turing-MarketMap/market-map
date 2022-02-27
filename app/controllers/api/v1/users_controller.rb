c2bc70cd21b13b33b4a46ee7de81ae7e919eb71a
Kevin Gloss <kgloss89@gmail.com>
2022-02-26 17:16:11 -0700

    Create user controller/spec placeholders

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,3 +1,29 @@
 class Api::V1::UsersController < ApplicationController 
+  def create 
+    user = User.new(user_params)
 
+    if user.save 
+      json_response(UserSerializer.new(user), :created)
+    else
+      render json: user.errors, status: :unprocessable
+    end
+  end
+
+  def update 
+    @user.update!(user_params)
+    json_response(UserSerializer.new(@user))
+  end
+
+  def destroy 
+
+  end
+
+  private 
+    def user_params 
+      params.require(:user).permit(:first_name, :last_name, :email)
+    end
+
+    def find_user 
+      @user = User.find_by(email: params[:email])
+    end
 end
---
+++ spec/requests/api/v1/user_request_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe 'Users API', type: :request do 
+  it 'creates a new user' do
+
+  end
+
+  it 'can edit a user' do 
+    
+  end
+
+  it 'destroys a user' do 
+
+  end
+end
