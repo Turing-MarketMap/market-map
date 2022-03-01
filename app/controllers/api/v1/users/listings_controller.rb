9f0a1ed1a14fab30fd138d1d9c8feb3dc5d8ccc1
Kevin Gloss <kgloss89@gmail.com>
2022-02-28 19:30:13 -0700

    Add create outline

--- app/controllers/api/v1/users/listings_controller.rb
+++ app/controllers/api/v1/users/listings_controller.rb
@@ -1,9 +1,11 @@
 class Api::V1::Users::ListingsController < ApplicationController
   def index
-    # binding.pry
-    # binding.pry
     user = User.find(params[:user_id])
 
     json_response(ListingSerializer.new(user.listings))
+  end
+
+  def create 
+    binding.pry
   end
 end
