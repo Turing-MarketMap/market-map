403a4b8268a53223955493a5fc31172324424b9b
Kevin Gloss <kgloss89@gmail.com>
2022-02-28 20:57:12 -0700

    Clean up users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,24 +1,12 @@
 class Api::V1::UsersController < ApplicationController
-
-  #can we wrap the login feature into this as well? Does the session get created
-  # here or on the front end?
   def create
-    user = User.find_or_create_by(
-      email: user_params['email'],
-      first_name: user_params['first_name'],
-      last_name: user_params['last_name']
-    )
+    user = User.find_or_create_by(user_params)
 
     if user.valid?
       json_response(UserSerializer.new(user), :ok)
     else
       json_response(user.errors, :unprocessable_entity)
     end
-    # if user.save
-    #   json_response(UserSerializer.new(user), :ok)
-    # else
-    #   json_response(user.errors, :unprocessable_entity)
-    # end
   end
 
   def destroy
