5656efc31af7ddfdd947e22c24fd168a190bfe1a
Leland Curtis <lelandcurtis88@gmail.com>
2022-03-03 00:28:27 -0700

    Add comments.

--- app/poros/ml_model.rb
+++ app/poros/ml_model.rb
@@ -9,9 +9,11 @@ class MlModel
   def initialize(file_name, data = nil)
     @file_name = file_name
     if data && data[:odometer] && data[:sellingprice]
+      # get max from data for data normalization
       @input_max = data[:odometer].max.to_f
       @output_max = data[:sellingprice].max.to_f
 
+      #scale data: Default training settings requires that all values be normalized (between 0 and 1).
       @inputs = data[:odometer].to_a.map { |row| [row.to_f / input_max] }
       @outputs = data[:sellingprice].to_a.map { |row| [row.to_f / output_max] }
     end
@@ -19,12 +21,18 @@ class MlModel
   end
 
   def train(neurons = 200, epochs = 10000, report_rate = 100, mse_target = 0.001)
+    #Based on experience, a single layer of neurons will work fine for this kind of dataframe
+    # neurons = number in layer.
+    # ephocs = number of times we run training data through the network.
+    # Report rate = how many ephocs between accuracy CLI reports
+    # mse_target = error level at which the training automatically stops
     train_data = RubyFann::TrainData.new(inputs: @inputs, desired_outputs: @outputs)
     @net = RubyFann::Standard.new(num_inputs: 1, hidden_neurons: [neurons], num_outputs: 1)
     @net.train_on_data(train_data, epochs, report_rate, mse_target)
   end
 
   def save
+    # save the nueral net and the max data in order to scale predictions from 0-1 to a more meaningful value
     model_location = "db/ml/v1/models"
     max_data_location = "db/ml/v1/max_data"
     @net.save("#{model_location}/#{@file_name}.net")
@@ -37,13 +45,16 @@ class MlModel
   end
 
   def predict_price_from_mileage(input)
+    # normalize input values, predict, and then scale up to match original data
     scaled_input = input.to_f/@input_max
     predictions = @net.run([scaled_input])
-    predictions.map { |prediction| prediction * @output_max} 
+    predictions.map { |prediction| prediction * @output_max}
   end
 
-  # load trained model and max_data and create new MlModel object.
+
   def self.load_MlModel(file_name)
+    # load trained model and max_data and return new MlModel object.
+    # this object will not have the raw data used for training, but it will be able to make predictions.
     model_location = "db/ml/v1/models"
     max_data_location = "db/ml/v1/max_data"
 
