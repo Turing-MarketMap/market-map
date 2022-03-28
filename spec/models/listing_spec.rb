741692f2a7edab9025ed2be13c9cb5085068bc76
Leland Curtis <lelandcurtis88@gmail.com>
2022-03-28 14:10:50 -0600

    Remove validations to allow new listings with only critical inputs.

--- spec/models/listing_spec.rb
+++ spec/models/listing_spec.rb
@@ -24,18 +24,8 @@ RSpec.describe Listing do
     it { should validate_numericality_of(:year).only_integer.is_greater_than(0) }
     it { should validate_presence_of :make }
     it { should validate_presence_of :model }
-    it { should validate_presence_of :trim }
-    it { should validate_presence_of :body }
-    it { should validate_presence_of :transmission }
-    it { should validate_presence_of :vin }
-    it { should validate_presence_of :state }
-    it { should validate_presence_of :condition }
-    it { should validate_numericality_of(:condition).is_greater_than_or_equal_to(0) }
-    it { should validate_numericality_of(:condition).is_less_than_or_equal_to(5) }
     it { should validate_presence_of :odometer }
-    it { should validate_numericality_of(:odometer).only_integer.is_greater_than(0) }
-    it { should validate_presence_of :color }
-    it { should validate_presence_of :interior }
+    it { should validate_numericality_of(:odometer).only_integer.is_greater_than_or_equal_to(0) }
     it { should validate_presence_of :sellingprice }
     it { should validate_numericality_of(:sellingprice).only_integer.is_greater_than(0) }
   end
